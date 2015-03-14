870444704646be6bbf7b23e9521789bc20ce8905
Matěj Kocián <matej.kocian@gmail.com>
2015-03-14 02:11:20 +0100

    Computing DTW DP table in window only
    Computation sped up by a factor of
        ~7 for window tolerance = 0.2
        ~12 for window tolerance = 0.1

--- dtw.m
+++ dtw.m
@@ -9,75 +9,57 @@ function [y, path] = dtw(A, B)
     % table for dynamic programming
     tab = zeros(lA, lB) + Inf;
 
-    % saving path
-    path = zeros(lA, lB);
-    % left, upperleft, upper
-    directions = [[0, -1]; [-1, -1]; [-1, 0]];
-
     tab(1,1) = distance(A(1, :), B(1, :));
+    tol = 0.2;
+
+
+    % first row
+    for b = 2:min(lB, floor((lB / lA) + lB*tol))
+        tab(1, b) = distance(A(1, :), B(b, :)) + tab(1, b-1);
+    end
+
 
     % general case
-    for a = 1:lA
-        for b = 1:lB
-            
-            if (a == 1) && (b == 1)
-                continue;
-            end
+    for a = 2:lA
+        
+        % only window
+        from = max(1, ceil(a * (lB / lA) - lB*tol));
+        to = min(lB, floor(a * (lB / lA) + lB*tol));
+
+        
+        % first valid position on the row
+        b = from;
+        upper = tab(a-1, b);
+        upperleft = Inf;
+        if dtw_in(a-1, b-1, lA, lB)
+            upperleft = tab(a-1, b-1);
+        end
+
+        % weight coefficient -- manhattan distance from last point
+        w = 1 + (upperleft < upper);
+
+        tab(a, b) = w * distance(A(a, :), B(b, :)) + min([upper, upperleft]);
+
+
+        for b = (from+1):to
             
             % using adjustment window -- only take the positions inside the
             % window into consideration
-            left = Inf;
-            if dtw_in(a, b-1, lA, lB)
-                left = tab(a, b-1);
-            end
-
-            upper = Inf;
-            if dtw_in(a-1, b, lA, lB)
-                upper = tab(a-1, b);
-            end
-
-            upperleft = Inf;
-            if dtw_in(a-1, b-1, lA, lB)
-                upperleft = tab(a-1, b-1);
-            end
+            left = tab(a, b-1);
+            upper = tab(a-1, b);
+            upperleft = tab(a-1, b-1);
 
             % weight coefficient -- manhattan distance from last point
-            w = 1;
-            if upperleft < min([upper, left])
-                w = 2;
-            end
+            w = 1 + (upperleft < min([upper, left]));
 
             tab(a, b) = w * distance(A(a, :), B(b, :)) + min([upper, left, upperleft]);
-
-            if left <= min([upper, upperleft])
-                path(a, b) = 1;
-            end
-            if upperleft < min([left, upper])
-                path(a, b) = 2;
-            end
-            if upper <= min([left, upperleft])
-                path(a, b) = 3;
-            end
 
         end
     end
 
     % value of the best alignment of the two sequences
     % normalized
-            
+
     y = tab(lA, lB) / (lA + lB);
-
-
-    path(1, 1) = 99;
-    
-    a = lA;
-    b = lB;
-    while path(a, b) ~= 99
-        tmp = path(a, b);
-        path(a, b) = 99;
-        tmp = directions(tmp, :);
-        a = a + tmp(1);
-        b = b + tmp(2);
-    end
 end
 
