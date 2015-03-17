cb41a3dd5cbd0c2ca5db312b55fa331aa58dc10a
Adam Vyskovsky <adamvyskovsky@gmail.com>
2015-03-17 10:01:37 +0100

    Min dist between digit and set of digits

---
+++ min_distance.m
@@ -0,0 +1,8 @@
+% The shortest distance from a to some digit in digits
+function y = min_distance(a, digits)
+    m = Inf;
+    for i = 1:length(digits)
+        m = min(m, dtw(a, digits{i});
+    end
+    y = m;
+end
